eaa9926df660bd6142f264b472794ab33cca7cb6
Thabhelo Duve <thabheloduve@gmail.com>
2024-01-24 06:00:26 -0600

    Initial commit all

--- components/errorNoFooter.tsx
+++ components/errorNoFooter.tsx
@@ -2,9 +2,9 @@ const ErrorNoFooter = () => {
   return (
     <div className='max-w-5xl mx-auto px-6 py-20 text-gray-600'>
       <h2 className='text-2xl text-red-600 font-semibold mb-8'>
-        Warning: there is no footer.
+        thabhelo2024
       </h2>
-      <p className='mb-6'>React Bricks cannot find an entity for the footer.</p>
+      <p className='mb-6'>Thank you for visiting!.</p>
     </div>
   )
 }

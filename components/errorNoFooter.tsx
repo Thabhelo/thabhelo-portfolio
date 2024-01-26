c06bf991f398982988e17a225262ed4d1300ffd4
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2024-01-25 22:50:37 -0600

    Update errorNoFooter.tsx
    Added copyright infomation

--- components/errorNoFooter.tsx
+++ components/errorNoFooter.tsx
@@ -2,7 +2,7 @@ const ErrorNoFooter = () => {
   return (
     <div className='max-w-5xl mx-auto px-6 py-20 text-gray-600'>
       <h2 className='text-2xl text-red-600 font-semibold mb-8'>
-        thabhelo2024
+        <p>&copy; 2024 Thabhelo Duve. All rights reserved.</p>
       </h2>
       <p className='mb-6'>Thank you for visiting!.</p>
     </div>
